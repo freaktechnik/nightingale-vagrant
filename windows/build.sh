3355f19b210943864f1d1a2a373f0d50cd8eb359
freaktechnik <martin.a.giger@sunrise.ch>
2015-05-14 15:31:10 +0200

    Remove custom build.sh

--- windows/build.sh
+++
@@ -1,4 +0,0 @@
-#! C:\mozilla-build\msys\bin\bash
-TEMP?=C:\tmp
-cd "$TEMP\nightingale-hacking"
-./build.sh
--- windows/setup_box.bat
+++ windows/setup_box.bat
@@ -2,7 +2,8 @@ rem Install the deps that aren't on the VM yet.
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco install -y mozillabuild windows-sdk-7.0 git pstools visualstudioexpress2008
 
-set LOCALSRC=%TEMP%\nightingale-hacking
+rem Make sure %TEMP%, C:\temp and $TEMP in msys match. Or use the matching pairs.
+set LOCALSRC=C:\tmp\nightingale-hacking
 
 rem Copy the Nightingale source into a non-shared folder
 xcopy /y/s/e/k/z/i "C:\nightingale-hacking" %LOCALSRC%
@@ -15,7 +16,7 @@ echo 'ac_add_options --enable-installer' >> %LOCALSRC%\nightingale.config
 echo 'ac_add_options --with-msvc-express' >> %LOCALSRC%\nightingale.config
 
 rem BUILD, run as user, since else msvc craps itself and displays unrelated errors.
-psexec -h -u vagrant -p vagrant -accepteula "C:\vagrant\start-msvc9.bat"
+cmd /c C:\vagrant\start-msvc9.bat
 
 rem Sync the build and installer to the host machine
 xcopy /y/s/e/k/z/i %LOCALSRC%\compiled\dist "C:\vagrant\dist"
