bf7d9d588410694f66d93a6b1ba03fb16c80a32c
Martin Giger <martin.a.giger@sunrise.ch>
2015-05-15 15:01:35 +0200

    Fix windows. Like fo real

--- windows/setup_box.bat
+++ windows/setup_box.bat
@@ -3,20 +3,19 @@ rem Install the deps that aren't on the VM yet.
 choco install -y mozillabuild windows-sdk-7.0 git pstools visualstudioexpress2008
 
 rem Make sure %TEMP%, C:\temp and $TEMP in msys match. Or use the matching pairs.
-set LOCALSRC=C:\tmp\nightingale-hacking
+set LOCALSRC=%TEMP%\nightingale-hacking
 
 rem Copy the Nightingale source into a non-shared folder
 xcopy /y/s/e/k/z/i "C:\nightingale-hacking" %LOCALSRC%
 
 rem Add system-specific build config settings
-echo 'ac_add_options --with-taglib-source=packaged' >> %LOCALSRC%\nightingale.config
-echo 'ac_add_options --disable-compiler-environment-checks' >> %LOCALSRC%\nightingale.config
-echo 'ac_add_options --with-media-core=default' >> %LOCALSRC%\nightingale.config
-echo 'ac_add_options --enable-installer' >> %LOCALSRC%\nightingale.config
-echo 'ac_add_options --with-msvc-express' >> %LOCALSRC%\nightingale.config
+echo ac_add_options --disable-compiler-environment-checks >> %LOCALSRC%\nightingale.config
+echo ac_add_options --with-media-core=default >> %LOCALSRC%\nightingale.config
+echo ac_add_options --enable-installer >> %LOCALSRC%\nightingale.config
+echo ac_add_options --with-msvc-express >> %LOCALSRC%\nightingale.config
 
-rem BUILD, run as user, since else msvc craps itself and displays unrelated errors.
-cmd /c C:\vagrant\start-msvc9.bat
+rem BUILD, run as user, else the environement is somehow broken. The -h option is needed just to get the console output within the vagrant provisioner, nothing in the build should require the elevated rights.
+psexec -accepteula -u vagrant -p vagrant -h "C:\vagrant\start-msvc9.bat"
 
 rem Sync the build and installer to the host machine
 xcopy /y/s/e/k/z/i %LOCALSRC%\compiled\dist "C:\vagrant\dist"
